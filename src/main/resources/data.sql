f6c74cd79f789a4f9006172522f10678bcaa78c9
Kevin Matheo Paspuel <atheoos@gmail.com>
2024-02-01 00:02:53 -0500

    Se agrega archivo sql en resources

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+/*INSERT INTO maturity (id, maturityLevel, maturityDescription) VALUES (1, 1, 'TODOPUBLICO');
+INSERT INTO maturity (id, maturityLevel, maturityDescription) VALUES (2, 2, 'SUPERVISION');
+INSERT INTO maturity (id, maturityLevel, maturityDescription) VALUES (3, 3, 'ADULTOS');
+
+INSERT INTO image (id, videoId, url, TYPE, width, height, extension, SIZE, imageKey) VALUES (1,1, 'http://image1', 'img', 100, 100, 'jpg', '100', '1');
+INSERT INTO image (id, videoId, url, TYPE, width, height, extension, SIZE, imageKey) VALUES (2,2, 'http://image2', 'img', 200, 200, 'jpg', '200', '2');
+
+INSERT INTO gender (id, nameGender) VALUES (1, 'KIDS');
+INSERT INTO gender (id, nameGender) VALUES (2, 'TERROR');
+INSERT INTO gender (id, nameGender) VALUES (3, 'DRAMA');
+
+INSERT INTO movies (id, videoId, releaseYear, title, synopsis, numSeasonsLabel, id_gender, id_image, id_maturity ) VALUES (1, 1, 2020, 'TEST1', 'TESTDES1', 1, 1, 1, 1);
+INSERT INTO movies (id, videoId, releaseYear, title, synopsis, numSeasonsLabel, id_gender, id_image, id_maturity ) VALUES (2, 2, 2021, 'TEST2', 'TESTDES2', 1, 2, 2, 2);
+INSERT INTO movies (id, videoId, releaseYear, title, synopsis, numSeasonsLabel, id_gender, id_image, id_maturity ) VALUES (3, 3, 2022, 'TEST3', 'TESTDES3', 1, 3, 1, 3);
+*/
